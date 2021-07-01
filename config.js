d8a5bc1f1c74292dcdb7b7c030bc7f89f75e3f8c
rintheki24 <86769589+rintheki24@users.noreply.github.com>
2021-07-01 15:23:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,15 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng em đã yêu ngay sau vài câu thả thính 😙',
     desc: 'liệu cá có iu tứn không ',
     btnYes: 'iu tứn <33',
     btnNo: 'hong, tứn à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay làm sp anh tiếp :3.',
     btnAccept: 'Uciiiii lun <3',
-    messLink: 'https://www.facebook.com/' //https://www.facebook.com/messages/t/100048707880914}
+    messLink: 'http://fb.com/' //https://www.messenger.com/t/100036792811356
+}
