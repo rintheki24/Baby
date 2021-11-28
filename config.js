491e873a6bb1598f495501c403d34c75bb161c4e
rintheki24 <86769589+rintheki24@users.noreply.github.com>
2021-11-28 18:44:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay leo rank với anh tiếp nha :3.',
     btnAccept: 'Dạaaaaa <3',
-    messLink: 'https://fb.com//https://www.messenger.com/t/100036791652510
+    messLink: 'https://fb.com//www.messenger.com/t/100036791652510
 }
